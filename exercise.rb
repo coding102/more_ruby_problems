a35c51314918820f2e87f2bed72acb59f5ebb7a4
Marck <mercercountynj@gmail.com>
2015-12-16 14:11:33 -0500

    exercises

---
+++ exercise.rb
@@ -0,0 +1,38 @@
+
+
+#def countdown(n)
+#    return if n.zero? # base case
+#  puts n
+#  countdown(n-1)    # getting closer to base case 
+#end               
+#
+#countdown(100)
+
+
+
+
+
+
+#def choose (x)
+#    #convert x to an integer
+#    x = x.to_i
+#    return 0 if x % 13 == 0
+#    puts "choose again"
+#    choose (gets.chomp)
+#    
+#end
+#
+#choose 3
+
+
+
+
+
+
+#def loop_sub(x)
+#    return x if x <= 0
+#    puts x
+#    loop_sub (x-1)
+#end
+#
+#    loop_sub 100
